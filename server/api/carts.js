3568725a0f84827c7d8ad4d667eb116c4566d18d
Yeshi Wangdi <ywblackberry@gmail.com>
2018-06-04 16:26:39 -0400

    cart's routes are now done

--- server/api/carts.js
+++ server/api/carts.js
@@ -10,3 +10,33 @@ router.get('/:userId', async (req, res, next) => {
       next(err)
   }
 })
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newCart = await Cart.create(req.body)
+    res.json(newCart)
+  } catch (err) {
+      next(err)
+  }
+})
+
+router.put('/:userId', async (req, res, next) => {
+  try {
+    const cart = await Cart.findById(req.params.userId)
+    const updatedCart = await cart.update(req.body)
+    res.json(updatedCart)
+  } catch (err) {
+      next(err)
+  }
+})
+
+router.delete('/:userId', async (req, res, next) => {
+  try {
+    const cart = await Cart.findById(req.params.userId)
+    await cart.destroy(req.body)
+    res.status(204).end()
+  } catch (err) {
+      next(err)
+  }
+})
+
