d9b7d1f949d67049097ca94e8fc80df901274dc2
Yeshi Wangdi <ywblackberry@gmail.com>
2018-06-04 15:50:58 -0400

    cart model complete

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -3,8 +3,23 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Cart = db.define('cart', {
-    
-
+    products: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        
+    },
+    subTotal: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    total: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    tax: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    }
 })
 
 
