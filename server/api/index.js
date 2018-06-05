ae059d1296392e10f8fa60e622178e9854df2c22
Jay.Lee <kkomarocker@gmail.com>
2018-06-05 15:08:12 -0400

    Test passed

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ module.exports = router;
 
 router.use('/users', require('./users'));
 router.use('/carts', require('./carts'));
+router.use('/cars', require('./cars'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found');
---
+++ server/db/models/car.spec.js
@@ -0,0 +1,54 @@
+/* global describe beforeEach it */
+
+const { expect } = require('chai');
+const db = require('../index');
+const Car = db.model('car');
+
+describe('Car model', () => {
+  beforeEach(() => {
+    return db.sync({ force: true });
+  });
+
+  describe('creates vehicle information and saves into database', () => {
+    describe('car model', () => {
+      let car;
+
+      beforeEach(() => {
+        return Car.create({
+          name: 'Corvette',
+          model: 'ZF1',
+          year: 2018,
+          color: 'Ghost white',
+          description: 'Brand New',
+          specification: 'V20 engine',
+          price: 100000,
+          rating: [5],
+          imageUrl:
+            'http://www.stingrayforums.com/forum/attachments/purchasing/13098d1411290857-finally-got-my-2015-c7-arctic-white-great-site-great-people-zf1.1.jpg',
+          country: 'USA'
+        }).then(vehicle => {
+          car = vehicle;
+        });
+      });
+
+      it('name, model, color, desc, spec, img, country to be a string', () => {
+        expect(typeof car.name).to.be.equal('string');
+        expect(typeof car.model).to.be.equal('string');
+        expect(typeof car.color).to.be.equal('string');
+        expect(typeof car.description).to.be.equal('string');
+        expect(typeof car.specification).to.be.equal('string');
+        expect(typeof car.imageUrl).to.be.equal('string');
+        expect(typeof car.country).to.be.equal('string');
+      });
+
+      it('year and price to be a number', () => {
+        expect(typeof car.year).to.deep.equal('number');
+        expect(typeof car.price).to.deep.equal('number');
+      });
+
+      it('requires rating to be an array', () => {
+        expect(Array.isArray(car.rating)).to.be.equal(true);
+      });
+    }); // end describe('correctPassword')
+  }); // end describe('instanceMethods')
+}); // end describe('User model')
