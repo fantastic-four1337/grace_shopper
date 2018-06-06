586ff7de5c1a9066649d7f46a407fa3592c435f7
Yeshi Wangdi <ywblackberry@gmail.com>
2018-06-06 17:29:51 -0400

    checkOut component is complete

--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,7 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
+import checkOut from '../client/components/checkOut'
 
 /**
  * COMPONENT
@@ -21,8 +22,9 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route exact path="/cars" component={AllCars} />
-        <Route path="/cars/:carId" component={SingleCar} />
+        {/* <Route exact path="/cars" component={AllCars} />
+        <Route path="/cars/:carId" component={SingleCar} /> */}
+        <Route path="/checkout" component={checkOut} />
         {
           isLoggedIn &&
             <Switch>
