c858be76d9854c2eb940868a6d881bafbca053d2
Daniel Howard <danielhoward314@gmail.com>
2018-06-06 17:38:44 -0400

    feat(cart component) creates the cart component

--- client/routes.js
+++ client/routes.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
+import Cart from '../client/components/cart'
 import {me} from './store'
 
 /**
@@ -21,8 +22,9 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route exact path="/cars" component={AllCars} />
-        <Route path="/cars/:carId" component={SingleCar} />
+        {/* <Route exact path="/cars" component={AllCars} />
+        <Route path="/cars/:carId" component={SingleCar} /> */}
+        <Route path="/cart" component={Cart} />
         {
           isLoggedIn &&
             <Switch>
