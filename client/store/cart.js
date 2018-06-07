82eaa664d1147645562016a580ccd1cb140e6768
Jay.Lee <kkomarocker@gmail.com>
2018-06-07 15:11:45 -0400

    feat(client/reducer): cart reducer added / chore(client/reducer): car reducer modified

---
+++ client/store/cart.js
@@ -0,0 +1,63 @@
+import CARTS from '../actionTypes/cart';
+
+const initialState = {
+  carts: [],
+  cart: {},
+  status: ''
+};
+
+export default function(state = initialState, action) {
+  const payload = action.payload;
+
+  switch (action.type) {
+    case CARTS.GOT_CARTS: {
+      return {
+        ...state,
+        carts: payload.instance
+      };
+    }
+    case CARTS.GOT_SINGLE_CART: {
+      return {
+        ...state,
+        cart: payload.selectedCart,
+        status: payload.status
+      };
+    }
+    case CARTS.ADDED_CART: {
+      return {
+        ...state,
+        carts: [...state.carts, payload.selectedCart],
+        status: payload.status
+      };
+    }
+    case CARTS.REMOVED_CART: {
+      return {
+        ...state,
+        carts: state.carts.filter(cart => {
+          return cart.id !== payload.id;
+        })
+      };
+    }
+    case CARTS.EDITED_CART: {
+      return {
+        ...state,
+        carts: [
+          ...state.carts.filter(cart => {
+            return cart.id !== payload.id;
+          }),
+          payload.selectedCart
+        ],
+        status: payload.status
+      };
+    }
+    case CARTS.REQUESTED_CART: {
+      return {
+        ...state,
+        status: payload.status
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
